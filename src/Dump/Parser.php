597c3e8d6073d45ec8e4121ebcb7422a7cde490d
Travis Hyyppä <travishyyppa@gmail.com>
2015-03-02 09:09:34 -0700

    Quick docblock
    Signed-off-by: Travis Hyyppä <travishyyppa@gmail.com>

--- src/Dump/Parser.php
+++ src/Dump/Parser.php
@@ -4,6 +4,11 @@ use Thyyppa\Drifter\Table\Table;
 
 class Parser {
 
+    /**
+     * @param $data
+     *
+     * @return mixed
+     */
     public function get_tables( $data )
     {
         preg_match_all( '#(CREATE TABLE `([A-Za-z0-9_-]+)` \(([^;]*)\))#', $data, $tables, PREG_SET_ORDER );
